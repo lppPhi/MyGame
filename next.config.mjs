00e027869be50505437385b1826ef9a04bab850a
lppPhi <lppPhi.1701@gmail.com>
2025-04-22 10:59:12 +0700

    PP

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true, // nếu bạn dùng <Image />
-  },
-  distDir: 'out', // thư mục build ra HTML tĩnh
-  basePath: '/MyGame', // TÊN repo GitHub của bạn
-  assetPrefix: '/MyGame/', // giúp load đúng asset tĩnh
 };
 
 export default nextConfig;
